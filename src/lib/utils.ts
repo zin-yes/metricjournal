87dc8eff59bfc13a7b490851349d31425e568e47
zin <juztz1n@gmail.com>
2024-10-25 17:43:09 +0200

    get base url change

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,6 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getBaseUrl() {
   if (typeof window !== "undefined") return window.location.origin;
-  if (process.env.CF_PAGES_URL) return `https://${process.env.CF_PAGES_URL}`;
-  return `http://localhost:${process.env.PORT ?? 3000}`;
+
+  return process.env.NODE_ENV === "production"
+    ? `https://metricjournal.com`
+    : `http://localhost:${process.env.PORT ?? 3000}`;
 }
